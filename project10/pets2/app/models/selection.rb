58592f5308b3d6ac83de9baaa2f936694c86787d
Andrew Reisdorph <areisdor@mymail.mines.edu>
2014-03-24 18:42:50 -0600

    Fixed a bug where a user could consider a pet multiple times. This new and improved code will only allow you to consider a pet once. If you want to consider multiple pets, you can still do this.

--- project10/pets2/app/models/selection.rb
+++ project10/pets2/app/models/selection.rb
@@ -2,7 +2,10 @@ class Selection < ActiveRecord::Base
 	has_many :adopted_pets, dependent: :destroy
   
   def add_cat(cat_id)
-    current_cat = adopted_pets.build(cat_id: cat_id)
-    current_cat
-  end
+  	current_cat = adopted_pets.find_by(cat_id: cat_id)
+    unless current_cat
+    	current_cat = adopted_pets.build(cat_id: cat_id)
+    end
+ end
+
 end
