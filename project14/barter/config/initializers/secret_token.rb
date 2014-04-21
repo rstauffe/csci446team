f3b8a8cf304c2ce9fc2d6fa7a3683b20351e5b20
Ryan Stauffer <rstauffe@mymail.mines.edu>
2014-04-21 17:25:55 -0600

    initialization of barter project

---
+++ project14/barter/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Barter::Application.config.secret_key_base = '47fd4000f33c928cbcd610349d005f221f109a3f5dcf50f9b3e95259681c4d3cdf6027f46c1e69770a9689b16702631feb55d35d678ceff113227685631fe315'
